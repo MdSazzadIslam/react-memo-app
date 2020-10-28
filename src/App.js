245a93fb96dcf39c74aafca1be532b053733fc26
Sazzad Islam <net_sazzad@outlook.com>
2020-10-28 23:08:20 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import http from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [error, setError] = useState(false);
 
-export default App;
+  const [records, setRecords] = useState([]);
+
+  useEffect(() => {
+    debugger;
+
+    http
+      .get("https://www.anapioficeandfire.com/api/books")
+
+      .then((response) => {
+        setError(false);
+        setRecords(response.data);
+        console.log(response.data);
+      })
+
+      .catch((error) => {
+        console.log(error);
+
+        setError(error);
+      });
+  }, [setRecords]);
+
+  if (error) {
+    return <div> Error: {error.message}</div>;
+  } else {
+    return (
+      <div className="App">
+        <table>
+          <thead>
+            <tr>
+              <th>SL</th>
+              <th>AUTHOR</th>
+              <th>NAME OF BOOK</th>
+              <th>PUBLISHER</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {records.map((item, key) => (
+              <tr key={key}>
+                <td> {key + 1}</td>
+                <td> {item.authors}</td>
+                <td> {item.name} </td>
+                <td> {item.publisher} </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+};
+
+export default React.memo(App);
