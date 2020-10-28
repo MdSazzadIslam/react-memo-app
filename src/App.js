650116d13ce5b214f290a275566385825ee1d56e
Sazzad Islam <net_sazzad@outlook.com>
2020-10-28 23:18:32 +0600

    App Updated

--- src/App.js
+++ src/App.js
@@ -57,3 +57,13 @@ const App = () => {
 };
 
 export default React.memo(App);
+
+/* When deciding to update DOM, React first renders your component, 
+then compares the result with the previous render result. 
+If the render results are different, React updates the DOM.
+Current vs previous render results comparison is fast. 
+But you can speed up the process under some circumstances.
+When a component is wrapped in React.memo(), 
+React renders the component and memoizes the result. 
+Before the next render, if the new props are the same, 
+React reuses the memoized result skipping the next rendering. */
